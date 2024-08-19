8aac2855d01cc0858624a671b44fbd7b3733cf64
IMBKSM <117885263+pyksm@users.noreply.github.com>
2024-08-19 18:21:01 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11720. 숫자의 합/숫자의 합.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int n, sum=0;
+    
+    scanf("%d", &n);
+    
+    char num[n];
+    
+    scanf("%s", num);
+    
+    for(int i=0; i<n; i++) sum += num[i] - '0';
+    
+    printf("%d", sum);
+}
