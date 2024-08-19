f6305cd4addfd5ec70c9ab6f1dd6b57fe03382cc
IMBKSM <117885263+pyksm@users.noreply.github.com>
2024-08-19 18:22:53 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("         ,r'\"7\n"
+                "r`-_   ,'  ,/\n"
+                " \\. \". L_r'\n"
+                "   `~\\/\n"
+                "      |\n"
+                "      |");
+}
