80f7291289335765f286659e9163dcb0a53c6b80
IMBKSM <117885263+pyksm@users.noreply.github.com>
2024-08-19 18:21:12 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char word[21];
+    scanf("%s", word);
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n",word);
+    printf(":fan::fan::fan:");
+}
