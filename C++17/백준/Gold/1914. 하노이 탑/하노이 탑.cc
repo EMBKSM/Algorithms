bb574989536aff5f20d8594525bea9e8d1198ca8
IMBKSM <117885263+pyksm@users.noreply.github.com>
2024-08-21 20:11:27 +0900

    [Gold V] Title: 하노이 탑, Time: 116 ms, Memory: 2208 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1914. 하노이 탑/하노이 탑.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+
+void hanoi(int n, int A, int C, int B)
+{
+    if (n == 1) {
+        printf("%d %d\n", A, C);
+        return;
+    }
+    hanoi(n - 1, A, B, C);
+    printf("%d %d\n", A, C);
+    hanoi(n - 1, B, C, A);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    string m = to_string(pow(2, n));
+
+    int x = m.find('.');				
+    m = m.substr(0, x);				
+    m[m.length() - 1] -= 1;
+    
+    cout << m << endl;
+    if (n <= 20) {
+        hanoi(n, 1, 3, 2);
+    }
+}
