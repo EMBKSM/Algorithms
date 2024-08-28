56d99154c433a7d15937ea1fac73928463babcd1
IMBKSM <117885263+IMBKSM@users.noreply.github.com>
2024-08-28 14:31:36 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len, int n) {
+    return num_list + (n - 1);
+}
