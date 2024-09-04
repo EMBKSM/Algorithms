2fd6a7642ee8aae26efce91dc8b11c4a86cb5263
EMBKSM <117885263+EMBKSM@users.noreply.github.com>
2024-09-04 13:56:52 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    for(let i = 0; i < num_list.length ; i++){
+        if(num_list[i] < 0){
+            return i;
+        }
+    }
+    return -1
+    return answer;
+}
