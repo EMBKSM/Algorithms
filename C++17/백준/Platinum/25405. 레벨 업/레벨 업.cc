d49e360e0596a54ea4d94e22e2c941ac803c5a5c
김성민 <117885263+EMBKSM@users.noreply.github.com>
2025-01-03 16:58:17 +0900

    [Platinum I] Title: 레벨 업, Time: 32 ms, Memory: 4472 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Platinum/25405. 레벨 업/레벨 업.cc
+++ C++17/백준/Platinum/25405. 레벨 업/레벨 업.cc
@@ -1,117 +1,63 @@
-#include <stdio.h>
-#include <algorithm>
-#define NMAX 100010
-#define INF 0x7FFFFFFFFFFFFFFF
-typedef long long int lint;
+#include <bits/stdc++.h>
+
 using namespace std;
- 
-int N, M, K;
-lint inp[NMAX];
- 
-lint l, r, idx, Bsz;
-lint a, b, valL, valR;
-lint ll, rr, mid, cur;
- 
+
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    scanf("%d", &N);
-    for(int i=0;i<N;i++) scanf("%lld", &inp[i]);
-    scanf("%d %d", &M, &K);
- 
-    sort(inp, inp+N);
- 
+    long long n, m, k;
+    cin >> n;
+    vector<long long> num_list(n);
 
-    l = r = K-1;
-    while(0<l or r<N-1) {
-        Bsz = (r-l)+1;
- 
+    for (long long i = 0; i < n; i++) {
+        cin >> num_list[i];
+    }
 
-        valL = INF;
-        if(l > 0) {
-            ll = 0; rr = M;
-            while(ll<=rr) {
-                mid = (ll+rr)/2;
- 
-        
-                a = (inp[l-1]+cur) + mid;
-                b = inp[l] + (idx + (K-l)*mid)/Bsz;
- 
-                if(a < b) ll = mid+1;
-                else {
-                    if(a == b) valL = min( valL, mid );
-                    rr = mid-1;
-                }
-            }
+    cin >> m >> k;
+    k--;
+
+    sort(num_list.begin(), num_list.end());
+
+    long long cnt = 1;
+    long long total = num_list[k] + m;
+    long long left = k, right = k;
+    long long crit = 0;
+
+    while (true) {
+        crit = total / cnt;
+        if (0 < left && crit < num_list[left - 1] + m) {
+            left--;
+            total += num_list[left] + m;
         }
- 
-   
-        valR = INF;
-        if(r < N-1) {
-            ll = 0; rr = M;
- 
-            while(ll<=rr) {
-                mid = (ll+rr)/2;
- 
-                a = inp[l] + (idx + (K-l)*mid + (Bsz-1))/Bsz;
-                b = inp[r+1];
- 
-                if(a < b) ll = mid+1;
-                else {
-                    if(a == b) valR = min( valR, mid );
-                    rr = mid-1;
-                }
-            }
+        else if (right < n - 1 && num_list[right + 1] <= crit) {
+            right++;
+            total += num_list[right];
         }
- 
-        
-        if(valL>M and valR>M) break;
         else {
-     
-            if(valL < valR) {
- 
-                inp[l-1] = inp[l-1] + cur + valL;
-                inp[r] = inp[l-1] + (idx + Bsz)/Bsz;
- 
-                idx = (idx + (K-l)*valL)%Bsz;
- 
-                cur += valL;
-                M -= valL;
-                l--;
-            }
-            else {
-                // 그룹 C에서 그룹 B로 가져오기
-                inp[l] = inp[l] + (idx + (K-l)*valR)/Bsz;
-                inp[r+1] = inp[l] + (idx+Bsz)/Bsz;
-
-                idx = (idx + (K-l)*valR)%Bsz;
-                if(idx>0) idx++;
- 
-                cur += valR;
-                M -= valR;
-                r++;
-            }
+            break;
         }
- 
+        cnt++;
     }
- 
-    if(M > 0) {
-        Bsz = (r-l)+1;
- 
-        inp[l] = inp[l] + (idx + (K-l)*M)/Bsz;
-        inp[r] = inp[l] + (idx + (Bsz-1))/Bsz;
- 
-        idx = (idx + (K-l)*M)%Bsz;
- 
-        cur += M;
-        M = 0;
+
+    vector<long long> answer;
+    for (int i = 0; i < n; i++) {
+        if (i < left) {
+            answer.push_back(num_list[i] + m);
+        }
+        else if (i <= right - (total % cnt)) {
+            answer.push_back(crit);
+        }
+        else if (i <= right) {
+            answer.push_back(crit + 1);
+        }
+        else {
+            answer.push_back(num_list[i]);
+        }
     }
- 
-    for(int i=0;i<=r;i++) {
-        if(i<l) inp[i] += cur;
-        else if(i<=r-idx) inp[i] = inp[l];
-        else inp[i] = inp[l]+1;
+
+    for (auto a : answer) {
+        cout << a << ' ';
     }
- 
-    for(int i=0;i<N;i++) printf("%lld ", inp[i]);
 }
- 
