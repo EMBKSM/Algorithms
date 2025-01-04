f64eefd734cd653d66f9ef5e2b8fc2c6f5e63fc8
김성민 <117885263+EMBKSM@users.noreply.github.com>
2025-01-05 00:26:42 +0900

    [Gold V] Title: 하노이 탑, Time: 128 ms, Memory: 2212 KB -BaekjoonHub

--- C++20 (Clang)/백준/Gold/1914. 하노이 탑/하노이 탑.cc
+++ C++20 (Clang)/백준/Gold/1914. 하노이 탑/하노이 탑.cc
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 
-
 using namespace std;
 
-void hanoi(int n, int A, int C, int B)
-{
+void hanoi(int n, int A, int C, int B){    
     if (n == 1) {
-        printf("%d %d\n", A, C);
+        cout << A << " "<< C << "\n";
         return;
     }
     hanoi(n - 1, A, B, C);
-    printf("%d %d\n", A, C);
+    cout << A << " "<< C << "\n";
     hanoi(n - 1, B, C, A);
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
     int n;
     cin >> n;
-    string m = to_string(pow(2, n));
 
-    int x = m.find('.');				
-    m = m.substr(0, x);				
+    string m = to_string(pow(2, n));
+    int x = m.find('.');
+    m = m.substr(0, x);
     m[m.length() - 1] -= 1;
-    
-    cout << m << endl;
+
+    cout << m << "\n";
     if (n <= 20) {
         hanoi(n, 1, 3, 2);
     }
